1fcaa74b0d0efeaa14ee81629919895412e43fe7
EGC <egc1005@naver.com>
2023-04-03 09:56:11 +0900

    settings.py 호스트 변경02

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-zzm(^kn1w(y)-%p!0+0knr^ag%m(h*^eoe^=6ci4*p+5xblosy
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    ".ap-northeast-2.compute.amazonaws.com",
+    "ec2-13-124-144-252.ap-northeast-2.compute.amazonaws.com",
 ]
 
 
